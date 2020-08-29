13255739ab810de937cdf2b17b4579d3350ec4fc
helenkim9318 <67102402+helenkim9318@users.noreply.github.com>
2020-08-29 17:18:14 +0900

    pre-random

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.movieproject  # 'movieproject'라는 이름의 db를 만듭니다.
 
-import requests  # requests 라이브러리 설치 필요
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+import requests
 
 for page_num in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     url = "https://api.themoviedb.org/3/movie/upcoming?api_key=5f7bf7c14846dfb7be7dcb66ee46a81a&language=ko-KR&page=" + str(page_num)
@@ -13,15 +17,18 @@ for page_num in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     for movie in movie_result:
         title = movie['title']
         overview = movie['overview']
-        genre_ids = movie['genre_ids']
         poster_path = movie['poster_path']
         release_date= movie['release_date']
 
         movie = {
             'title': title,
             'overview': overview,
-            'genre_ids': genre_ids,
             'poster_path': poster_path,
             'release_date' : release_date
         }
         db.movies.insert_one(movie)
+
+@app.route('/movies', methods=['GET'])
+def read_movies():
+    movies_list=list(db.movies.find({}))
+    return jsonify({'result': 'success', 'movies_list':movies_list})
